7c751904885d7c5cef8b55f132d5648202b9ee29
Tobias Hegemann <tobias.hegemann@googlemail.com>
2021-02-12 16:14:48 +0100

    Fixed dependencies

--- i18n/DigitalStage_de.ts
+++ i18n/DigitalStage_de.ts
@@ -5,42 +5,36 @@
     <name>LoginPane</name>
     <message>
         <location filename="../src/loginpane.ui" line="14"/>
-        <location filename="../bin/DigitalStage_autogen/include/ui_loginpane.h" line="130"/>
         <location filename="../build/DigitalStage_autogen/include/ui_loginpane.h" line="130"/>
         <source>Digital Stage</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/loginpane.ui" line="48"/>
-        <location filename="../bin/DigitalStage_autogen/include/ui_loginpane.h" line="132"/>
         <location filename="../build/DigitalStage_autogen/include/ui_loginpane.h" line="132"/>
         <source>Password:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/loginpane.ui" line="68"/>
-        <location filename="../bin/DigitalStage_autogen/include/ui_loginpane.h" line="133"/>
         <location filename="../build/DigitalStage_autogen/include/ui_loginpane.h" line="133"/>
         <source>Sign in</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/loginpane.ui" line="75"/>
-        <location filename="../bin/DigitalStage_autogen/include/ui_loginpane.h" line="134"/>
         <location filename="../build/DigitalStage_autogen/include/ui_loginpane.h" line="134"/>
         <source>Email:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/loginpane.ui" line="88"/>
-        <location filename="../bin/DigitalStage_autogen/include/ui_loginpane.h" line="135"/>
         <location filename="../build/DigitalStage_autogen/include/ui_loginpane.h" line="135"/>
         <source>Please enter your email and password from test.digital-stage.org:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/loginpane.ui" line="95"/>
-        <location filename="../bin/DigitalStage_autogen/include/ui_loginpane.h" line="136"/>
         <location filename="../build/DigitalStage_autogen/include/ui_loginpane.h" line="136"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;No Account yet? &lt;a href=&quot;https://test.digital-stage.org/account/signup&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0068da;&quot;&gt;Sign up here&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
@@ -50,7 +44,6 @@
     <name>MainWindow</name>
     <message>
         <location filename="../src/mainwindow.ui" line="14"/>
-        <location filename="../bin/DigitalStage_autogen/include/ui_mainwindow.h" line="73"/>
         <location filename="../build/DigitalStage_autogen/include/ui_mainwindow.h" line="73"/>
         <source>digital-stage.org</source>
         <translation type="unfinished"></translation>
@@ -58,8 +51,6 @@
     <message>
         <location filename="../src/mainwindow.ui" line="31"/>
         <location filename="../src/mainwindow.ui" line="51"/>
-        <location filename="../bin/DigitalStage_autogen/include/ui_mainwindow.h" line="76"/>
-        <location filename="../bin/DigitalStage_autogen/include/ui_mainwindow.h" line="78"/>
         <location filename="../build/DigitalStage_autogen/include/ui_mainwindow.h" line="76"/>
         <location filename="../build/DigitalStage_autogen/include/ui_mainwindow.h" line="78"/>
         <location filename="../src/mainwindow.cpp" line="35"/>
@@ -68,21 +59,18 @@
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="41"/>
-        <location filename="../bin/DigitalStage_autogen/include/ui_mainwindow.h" line="74"/>
         <location filename="../build/DigitalStage_autogen/include/ui_mainwindow.h" line="74"/>
         <source>Log in</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="46"/>
-        <location filename="../bin/DigitalStage_autogen/include/ui_mainwindow.h" line="75"/>
         <location filename="../build/DigitalStage_autogen/include/ui_mainwindow.h" line="75"/>
         <source>Log out</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="56"/>
-        <location filename="../bin/DigitalStage_autogen/include/ui_mainwindow.h" line="77"/>
         <location filename="../build/DigitalStage_autogen/include/ui_mainwindow.h" line="77"/>
         <location filename="../src/mainwindow.cpp" line="37"/>
         <source>Logout</source>
