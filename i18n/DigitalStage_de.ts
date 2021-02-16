25480a3d9df147069a4de6003a9bbbbf478197fc
delude88 <tobias.hegemann@googlemail.com>
2021-02-16 15:04:02 +0100

    ubuntu: require not special version of nlohmann_json

--- i18n/DigitalStage_de.ts
+++ i18n/DigitalStage_de.ts
@@ -6,32 +6,32 @@
     <message>
         <location filename="../src/app.cpp" line="19"/>
         <source>Digital Stage</source>
-        <translation>Digitale Bühne</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/app.cpp" line="21"/>
         <source>Close</source>
-        <translation>Beenden</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/app.cpp" line="25"/>
         <source>Login</source>
-        <translation>Anmelden</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/app.cpp" line="31"/>
         <source>Open stage</source>
-        <translation>Bühne öffnen</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/app.cpp" line="33"/>
         <source>Logout</source>
-        <translation>Abmelden</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/app.cpp" line="115"/>
         <source>Unknown email or wrong password</source>
-        <translation>Unbekannte E-Mail oder falsches Passwort</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -40,75 +40,37 @@
         <location filename="../src/logindialog.ui" line="13"/>
         <location filename="../bin/DigitalStage_autogen/include/ui_logindialog.h" line="130"/>
         <source>Digital Stage</source>
-        <translation>Digitale Bühne</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/logindialog.ui" line="47"/>
         <location filename="../bin/DigitalStage_autogen/include/ui_logindialog.h" line="132"/>
         <source>Password:</source>
-        <translation>Passwort:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/logindialog.ui" line="67"/>
         <location filename="../bin/DigitalStage_autogen/include/ui_logindialog.h" line="133"/>
         <source>Sign in</source>
-        <translation>Anmelden</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/logindialog.ui" line="74"/>
         <location filename="../bin/DigitalStage_autogen/include/ui_logindialog.h" line="134"/>
         <source>Email:</source>
-        <translation>E-Mail:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/logindialog.ui" line="87"/>
         <location filename="../bin/DigitalStage_autogen/include/ui_logindialog.h" line="135"/>
         <source>Please enter your email and password from test.digital-stage.org:</source>
-        <translation>Bitte gebe deine E-Mail Adresse und Dein Passwort von live.digital-stage.org ein:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/logindialog.ui" line="94"/>
         <location filename="../bin/DigitalStage_autogen/include/ui_logindialog.h" line="136"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;No Account yet? &lt;a href=&quot;https://test.digital-stage.org/account/signup&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0068da;&quot;&gt;Sign up here&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Noch keinen Account? &lt;a href=&quot;https://test.digital-stage.org/account/signup&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0068da;&quot;&gt;Erstelle hier einen Account&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
-    </message>
-</context>
-<context>
-    <name>LoginPane</name>
-    <message>
-        <source>Digital Stage</source>
-        <translation type="obsolete">Digitale Bühne</translation>
-    </message>
-    <message>
-        <source>Password:</source>
-        <translation type="obsolete">Passwort:</translation>
-    </message>
-    <message>
-        <source>Sign in</source>
-        <translation type="obsolete">Anmelden</translation>
-    </message>
-    <message>
-        <source>Email:</source>
-        <translation type="obsolete">E-Mail:</translation>
-    </message>
-    <message>
-        <source>Please enter your email and password from test.digital-stage.org:</source>
-        <translation type="obsolete">Bitte gebe deine E-Mail Adresse und Dein Passwort von live.digital-stage.org ein:</translation>
-    </message>
-    <message>
-        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;No Account yet? &lt;a href=&quot;https://test.digital-stage.org/account/signup&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0068da;&quot;&gt;Sign up here&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="obsolete">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Noch keinen Account? &lt;a href=&quot;https://test.digital-stage.org/account/signup&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0068da;&quot;&gt;Erstelle hier einen Account&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
-    </message>
-</context>
-<context>
-    <name>MainWindow</name>
-    <message>
-        <source>Login</source>
-        <translation type="obsolete">Anmelden</translation>
-    </message>
-    <message>
-        <source>Logout</source>
-        <translation type="obsolete">Abmelden</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -116,12 +78,12 @@
     <message>
         <location filename="../src/main.cpp" line="19"/>
         <source>Systray</source>
-        <translation>Systemleiste</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="20"/>
         <source>I couldn&apos;t detect any system tray on this system.</source>
-        <translation>Leider ist Dein System für die Digitale Bühne ungeeignet, da es keine Systemleiste bietet.</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
