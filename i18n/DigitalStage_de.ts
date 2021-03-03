accb58a7434f0c79a31e502771f8802a9b0ed397
delude88 <tobias.hegemann@googlemail.com>
2021-03-03 20:53:54 +0100

    Fixed workflow

---
+++ i18n/DigitalStage_de.ts
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>App</name>
+    <message>
+        <location filename="../src/app.cpp" line="22"/>
+        <source>Digital Stage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app.cpp" line="24"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app.cpp" line="28"/>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app.cpp" line="34"/>
+        <source>Open stage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app.cpp" line="36"/>
+        <source>Logout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app.cpp" line="123"/>
+        <source>Unknown email or wrong password</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LoginDialog</name>
+    <message>
+        <location filename="../src/logindialog.ui" line="13"/>
+        <location filename="../bin/DigitalStage_autogen/include/ui_logindialog.h" line="130"/>
+        <location filename="../build/DigitalStage_autogen/include/ui_logindialog.h" line="130"/>
+        <location filename="../cmake-build-debug/DigitalStage_autogen/include/ui_logindialog.h" line="130"/>
+        <location filename="../cmake-build-release/DigitalStage_autogen/include/ui_logindialog.h" line="130"/>
+        <source>Digital Stage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/logindialog.ui" line="47"/>
+        <location filename="../bin/DigitalStage_autogen/include/ui_logindialog.h" line="132"/>
+        <location filename="../build/DigitalStage_autogen/include/ui_logindialog.h" line="132"/>
+        <location filename="../cmake-build-debug/DigitalStage_autogen/include/ui_logindialog.h" line="132"/>
+        <location filename="../cmake-build-release/DigitalStage_autogen/include/ui_logindialog.h" line="132"/>
+        <source>Password:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/logindialog.ui" line="67"/>
+        <location filename="../bin/DigitalStage_autogen/include/ui_logindialog.h" line="133"/>
+        <location filename="../build/DigitalStage_autogen/include/ui_logindialog.h" line="133"/>
+        <location filename="../cmake-build-debug/DigitalStage_autogen/include/ui_logindialog.h" line="133"/>
+        <location filename="../cmake-build-release/DigitalStage_autogen/include/ui_logindialog.h" line="133"/>
+        <source>Sign in</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/logindialog.ui" line="74"/>
+        <location filename="../bin/DigitalStage_autogen/include/ui_logindialog.h" line="134"/>
+        <location filename="../build/DigitalStage_autogen/include/ui_logindialog.h" line="134"/>
+        <location filename="../cmake-build-debug/DigitalStage_autogen/include/ui_logindialog.h" line="134"/>
+        <location filename="../cmake-build-release/DigitalStage_autogen/include/ui_logindialog.h" line="134"/>
+        <source>Email:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/logindialog.ui" line="87"/>
+        <location filename="../bin/DigitalStage_autogen/include/ui_logindialog.h" line="135"/>
+        <location filename="../build/DigitalStage_autogen/include/ui_logindialog.h" line="135"/>
+        <location filename="../cmake-build-debug/DigitalStage_autogen/include/ui_logindialog.h" line="135"/>
+        <location filename="../cmake-build-release/DigitalStage_autogen/include/ui_logindialog.h" line="135"/>
+        <source>Please enter your email and password from test.digital-stage.org:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/logindialog.ui" line="94"/>
+        <location filename="../bin/DigitalStage_autogen/include/ui_logindialog.h" line="136"/>
+        <location filename="../build/DigitalStage_autogen/include/ui_logindialog.h" line="136"/>
+        <location filename="../cmake-build-debug/DigitalStage_autogen/include/ui_logindialog.h" line="136"/>
+        <location filename="../cmake-build-release/DigitalStage_autogen/include/ui_logindialog.h" line="136"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;No Account yet? &lt;a href=&quot;https://test.digital-stage.org/account/signup&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0068da;&quot;&gt;Sign up here&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="19"/>
+        <source>Systray</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="20"/>
+        <source>I couldn&apos;t detect any system tray on this system.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
