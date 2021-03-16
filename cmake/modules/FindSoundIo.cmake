852c8c3442eeb3c8d58b0e39c1fcc4440d239249
Tobias Hegemann <tobias.hegemann@googlemail.com>
2021-03-16 02:09:18 +0100

    Enhanced FindSoundio

--- cmake/modules/FindSoundIo.cmake
+++ cmake/modules/FindSoundIo.cmake
@@ -1,16 +1,40 @@
-# Copyright (c) 2015 Andrew Kelley
-# This file is MIT licensed.
-# See http://opensource.org/licenses/MIT
+# - Try to find soundio
+# Once done, this will define
+#
+#  SOUNDIO_FOUND - system has soundio
+#  SOUNDIO_INCLUDE_DIRS - the soundio include directories
+#  SOUNDIO_LIBRARIES - link these to use soundio
 
-# SOUNDIO_FOUND
-# SOUNDIO_INCLUDE_DIR
-# SOUNDIO_LIBRARY
+# Use pkg-config to get hints about paths
 
-find_path(SOUNDIO_INCLUDE_DIR NAMES soundio/soundio.h)
+if (SOUNDIO_LIBRARIES AND SOUNDIO_INCLUDE_DIRS)
+    set(SOUNDIO_FOUND TRUE)
+else ()
+    find_package(PkgConfig QUIET)
+    if (PKG_CONFIG_FOUND)
+        pkg_check_modules(SOUNDIO_PKGCONF soundio)
+    endif (PKG_CONFIG_FOUND)
 
-find_library(SOUNDIO_LIBRARY NAMES soundio)
+    # Include dir
+    find_path(SOUNDIO_INCLUDE_DIR
+            NAMES soundio/soundio.h
+            PATHS ${SOUNDIO_PKGCONF_INCLUDE_DIRS}
+            )
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SoundIo DEFAULT_MSG SOUNDIO_LIBRARY SOUNDIO_INCLUDE_DIR)
+    # Library
+    find_library(SOUNDIO_LIBRARY
+            NAMES soundio
+            PATHS ${SOUNDIO_PKGCONF_LIBRARY_DIRS}
+            )
 
-mark_as_advanced(SOUNDIO_INCLUDE_DIR SOUNDIO_LIBRARY)
+    find_package(PackageHandleStandardArgs)
+    find_package_handle_standard_args(Soundio DEFAULT_MSG SOUNDIO_LIBRARY SOUNDIO_INCLUDE_DIR)
+
+    if (SOUNDIO_FOUND)
+        set(SOUNDIO_LIBRARIES ${SOUNDIO_LIBRARY})
+        set(SOUNDIO_INCLUDE_DIRS ${SOUNDIO_INCLUDE_DIR})
+    endif (SOUNDIO_FOUND)
+
+    # Mark the singular variables as advanced, to hide them of the GUI by default
+    mark_as_advanced(SOUNDIO_LIBRARY SOUNDIO_INCLUDE_DIR)
+endif ()
